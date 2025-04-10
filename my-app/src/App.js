12c4a0187acc5ac29107ba45a756241de67d3558
Amaka Onwuli <amonwuli@gmail.com>
2025-04-10 14:52:47 +0100

    added footer and updated link

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,5 +1,6 @@
 import Weather from './Weather'
 import './App.css';
+import Footer from './Footer';
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
         <h1>Weather App</h1>
         <Weather/>
       </header>
+      <Footer/>
     </div>
   );
 }
---
+++ my-app/src/Footer.js
@@ -0,0 +1,26 @@
+import React from "react";
+import "./Footer.css";
+
+export default function Footer() {
+  return (
+    <div className="Footer">
+      <p>
+        This project was coded by{" "}
+        <a href="https://github.com/MadameAmaka" target="_blank" rel="noreferrer">
+          {" "}
+          Amaka Onwuli{" "}
+        </a>{" "}
+        , it is open-sourced on{" "}
+        <a href="https://github.com/MadameAmaka/My-Weather-App" target="_blank" rel="noreferrer">
+          {" "}
+          Github
+        </a>{" "}
+        and hosted on{" "}
+        <a href="https://amakasweatherapp.netlify.app/" target="_blank" rel="noreferrer">
+          {" "}
+          Netlify{" "}
+        </a>
+      </p>
+    </div>
+  );
+}
